0b0a9ef28795e1545813eb88ce96190f29e3fdc4
mengfan <809146548@qq.com>
2018-04-25 05:56:38 -0700

    test

---
+++ build/CMakeFiles/hello_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_test.dir/main/main.cpp.o"
+  "CMakeFiles/hello_test.dir/hello/hello.cpp.o"
+  "hello_test.pdb"
+  "hello_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
